6c1448f3c1cab636ecdef65d96d4e8c167e2a6da
MohitGoel109 <goel0277@gmail.com>
2026-06-12 15:19:25 +0530

    learning react

---
+++ React - Day 1/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Home';
+
+const App = () => {
+  return (
+    <div>
+    <div>App</div>
+      <h1>This is a heading tag</h1>
+      <Home />
+    </div>
+  );
+};
+
+export default App
