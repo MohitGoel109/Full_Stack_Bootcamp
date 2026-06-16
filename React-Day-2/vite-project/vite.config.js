f5e1598b74dd653a2b981d9cb4be1f417c012511
MohitGoel109 <goel0277@gmail.com>
2026-06-16 09:58:39 +0530

    jb

--- React-Day-2/vite-project/vite.config.js
+++ React-Day-2/vite-project/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Full_Stack_Bootcamp/React-Day-2/vite-project/",
+  base: "/Full_Stack_Bootcamp/react-day-2/",
 })
