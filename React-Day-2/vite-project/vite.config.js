b3e4788d9dfb03f9cda7c5c3a2fccddf586303da
MohitGoel109 <goel0277@gmail.com>
2026-06-16 10:55:09 +0530

    fixed

--- React-Day-2/vite-project/vite.config.js
+++ React-Day-2/vite-project/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// IMPORTANT: repo name = Full_Stack_Bootcamp
 export default defineConfig({
   plugins: [react()],
-  base: "/Full_Stack_Bootcamp/react-day-2/",
+  base: '/Full_Stack_Bootcamp/',
 })
