921db3f09188f7c70ed24b83922ee229327b3b6b
MohitGoel109 <goel0277@gmail.com>
2026-06-30 13:37:58 +0530

    Update student.mongodb.js

--- Mongodb-D1/student.mongodb.js
+++ Mongodb-D1/student.mongodb.js
@@ -21,10 +21,37 @@ use("user")
 //      "email":"cdc@cdc.com"
 //    }
 // ])
+// db.student.insertOne({
+//     "name": "Alice Johnson",
+//     "rollNo": 12347,
+//     "section": "DS",
+//     "email": "alice@abc.com"
+// })
+// db.student.insertMany([
+//     {
+//         "name": "Bob Smith",
+//         "rollNo": 12348,
+//         "section": "FSD",
+//         "email": "bob@def.com"
+//     },
+//     {
+//         "name": "Charlie Brown",
+//         "rollNo": 12349,
+//         "section": "Aiml",
+//         "email": "charlie@ghi.com"
+//     }
+// ])
 
 ///////////////Read
-// db.student.find()
+db.student.find()
 //db.student.find({"section"})
 //db.student.find({"rollNo":{$lte:12345 }})
 //db.student.find(find{}, {name:1, section:1,})
 //db.student.find().skip(2).limit(2)
+
+//update
+//db.student.updateOne({"rollNo":12344},{$set:{"name":"John Smith","section":"FSD"}})
+
+//delete
+//db.student.deleteOne({"rollNo":12344})
+//db.student.deleteMany({"section":"DS"})
